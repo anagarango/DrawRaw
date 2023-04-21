5d5cec0447a23f5dd919c5a10582629f2b1c16fa
Ana Arango <aarango4@my.bcit.ca>
2023-04-20 23:22:01 -0700

    can draw with black ink

--- hooks/useDraw.ts
+++ hooks/useDraw.ts
@@ -1,20 +1,61 @@
-import { useEffect, useRef } from "react"
+import { useEffect, useRef, useState } from 'react'
 
-export const useDraw = () => {
+export const useDraw = (onDraw: ({ ctx, currentPoint, prevPoint }: Draw) => void) => {
+  const [mouseDown, setMouseDown] = useState(false)
 
-    const canvasRef = useRef<HTMLCanvasElement>(null)
+  const canvasRef = useRef<HTMLCanvasElement>(null)
+  const prevPoint = useRef<null | Point>(null)
 
+  const onMouseDown = () => setMouseDown(true)
 
-    useEffect(()=>{
+//   const clear = () => {
+//     const canvas = canvasRef.current
+//     if (!canvas) return
 
-        const handler = (e: MouseEvent) => {
-            console.log({x: e.clientX, y: e.clientY })
-        }
+//     const ctx = canvas.getContext('2d')
+//     if (!ctx) return
 
-        canvasRef.current?.addEventListener('mousemove', handler)
+//     ctx.clearRect(0, 0, canvas.width, canvas.height)
+//   }
 
-        return () => canvasRef.current?.addEventListener('mousemove', handler)
-    },[])
+  useEffect(() => {
+    const handler = (e: MouseEvent) => {
+      if (!mouseDown) return
+      const currentPoint = computePointInCanvas(e)
 
-    return {canvasRef}
+      const ctx = canvasRef.current?.getContext('2d')
+      if (!ctx || !currentPoint) return
+
+      onDraw({ ctx, currentPoint, prevPoint: prevPoint.current })
+      prevPoint.current = currentPoint
+    }
+
+    const computePointInCanvas = (e: MouseEvent) => {
+      const canvas = canvasRef.current
+      if (!canvas) return
+
+      const rect = canvas.getBoundingClientRect()
+      const x = e.clientX - rect.left
+      const y = e.clientY - rect.top
+
+      return { x, y }
+    }
+
+    const mouseUpHandler = () => {
+      setMouseDown(false)
+      prevPoint.current = null
+    }
+
+    // Add event listeners
+    canvasRef.current?.addEventListener('mousemove', handler)
+    window.addEventListener('mouseup', mouseUpHandler)
+
+    // Remove event listeners
+    return () => {
+      canvasRef.current?.removeEventListener('mousemove', handler)
+      window.removeEventListener('mouseup', mouseUpHandler)
+    }
+  }, [onDraw])
+
+  return { canvasRef, onMouseDown }
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,41 @@
-import { FC } from "react";
+import { FC } from 'react'
 import { useDraw } from '../../hooks/useDraw'
 
 interface pageProps {}
 
 const page: FC<pageProps> = ({}) => {
-  const {canvasRef} = useDraw()
+  const { canvasRef, onMouseDown } = useDraw(drawLine)
 
-  return <div className="w-screen h-screen flex justify-center items-center">
-    <canvas ref={canvasRef} width={750} height={750} className='border border-black rounded-md' />
+  function drawLine({ prevPoint, currentPoint, ctx }: Draw) {
+    const { x: currX, y: currY } = currentPoint;
+    const lineColor = "#000";
+    const lineWidth = 5;
+
+    let startPoint = prevPoint ?? currentPoint;
+    ctx.beginPath();
+    ctx.lineWidth = lineWidth;
+    ctx.strokeStyle = lineColor;
+    ctx.moveTo(startPoint.x, startPoint.y);
+    ctx.lineTo(currX, currY);
+    ctx.stroke();
+
+    ctx.fillStyle = lineColor;
+    ctx.beginPath();
+    ctx.arc(startPoint.x, startPoint.y, 2, 0, 2 * Math.PI);
+    ctx.fill();
+  }
+
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <canvas
+        onMouseDown={onMouseDown}
+        ref={canvasRef}
+        width={750}
+        height={750}
+        className="border border-black rounded-md"
+      />
     </div>
-}
+  );
+};
 
 export default page
