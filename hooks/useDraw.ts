e90291dcc71b016da2ec1aa808696dd16ab4682c
Ana Arango <aarango4@my.bcit.ca>
2023-04-20 22:25:39 -0700

    console logging the coordinates of mouse when hovering over canvas

---
+++ hooks/useDraw.ts
@@ -0,0 +1,20 @@
+import { useEffect, useRef } from "react"
+
+export const useDraw = () => {
+
+    const canvasRef = useRef<HTMLCanvasElement>(null)
+
+
+    useEffect(()=>{
+
+        const handler = (e: MouseEvent) => {
+            console.log({x: e.clientX, y: e.clientY })
+        }
+
+        canvasRef.current?.addEventListener('mousemove', handler)
+
+        return () => canvasRef.current?.addEventListener('mousemove', handler)
+    },[])
+
+    return {canvasRef}
+}
